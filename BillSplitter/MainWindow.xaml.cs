31059f8f2c04880ff53d925ebd602a008d1408f3
cybersi-io <66743889+cybersi-io@users.noreply.github.com>
2021-04-16 11:49:27 +0100

    Add project files.

---
+++ BillSplitter/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BillSplitter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            decimal bill = decimal.Parse(input_box.Text);
+            int tip_percent = int.Parse(tip_comboBox.Text);
+            int persons = int.Parse(persons_comboBox.Text);
+
+            decimal tip = decimal.Round(bill * tip_percent / 100 / persons, 2);
+            Console.WriteLine(tip);
+            decimal share = decimal.Round(bill / persons, 2);
+            decimal total = decimal.Round(share + tip, 2);
+
+            share_output.Text = share.ToString();
+            tip_output.Text = tip.ToString();
+            total_output.Text = total.ToString();
+
+        }
+    }
+}
